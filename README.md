9a49e4ec4d9cacae69ffd58ce673520a32a1ca49
Kiara <frida.shivaa@gmail.com>
2026-04-13 11:55:49 +0200

    add flower

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Art + Code
 
 Turtle Exploration
+
+### No module named 'tkinter'
+
+```bash 
+sudo apt-get install python3-tk
+```
+
+<details>
+<summary> UV Setup</summary>
+
+#### Create new UV
+
+```bash
+uv venv .venv
+```
+
+#### Activate UV
+
+##### Linux/Mac
+
+```bash
+source .venv/bin/activate
+```
+
+</details>
--- flower.py
+++ flower.py
@@ -1,20 +1,22 @@
 from turtle import *
-import random
-import colorsys as cs
 
 BG_COLOR= "#231C29"
-
 
 screen = Screen()
 screen.colormode(255)
 
 t1 = Turtle()
 t1.screen.bgcolor(BG_COLOR)
+t1.speed(13)
 
 
 for j in range(25):
     for i in range(15):
-        t1.color(cs.hsv_to_rgb(i/15, j/15,1))
+        r = int(((i/15)*255)%255)
+        g = int(((j/15)*255)%255)
+
+        color = (r, g,255)
+        t1.color(color)
         t1.right(90)
         t1.circle(200-j*4,90)
         t1.left(90)
