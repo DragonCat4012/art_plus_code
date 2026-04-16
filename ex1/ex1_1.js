a10217905d75e68a60b0bc614db4470e7119fb3e
Kiara <frida.shivaa@gmail.com>
2026-04-16 17:46:19 +0200

    Add exercise desc

--- ex1/ex1_1.js
+++ ex1/ex1_1.js
@@ -1,28 +1,58 @@
+/*
+Write functions for two basic shapes and place them in a grid.
+Vary position, rotation, scale, or the grid structure itself (irregularities, circle grid, etc.) t
+o make it compelling. Export 3 compositions,
+give them a matching title and note what parameters you can manipulate.
+*/
+
 Canvas.setpenopacity(1);
 
-const circleAccuracy = 360; // min=2 max=360 step=10  Circle accuracy
-const gridSize = 30; // min=30 max=100 step=10  Grid size
+const starAccuraccy = 6; // min=2 max=48 step=10  Circle accuracy
+const circleAccuracy = 12;
+const gridSize = 60; // grr
+
+const gridType = 0; // min=0, max=2, step=1, (1D-Grid, 2D-Grid, 3D-Grid)
 
 const startX = -100;
 const starY = -100;
 
 const fishX = 30; // min=-100 max=100 step=10  Fish X
 const fishY = 30; // min=-100 max=100 step=10  Fish Y
-const falloff = 200; // min=10 max=200 step=10 Fish Falloff
+const falloff = 58; // min=10 max=120 step=10 Fish Falloff
 const strength = 0.9; // min=0.1 max=1 step=0.1 Fish Strength
 
 // Global code will be evaluated once.
 const t = new Turtle();
 
+/*
+function createGrid() {
+  if (gridType == 0) {
+    draw(i, 20);
+  } else if (gridType == 1) {
+    for (let i = 0; i < gridSize; i++) {
+      for (let j = 0; j < gridSize; j++) {
+        draw(i, j);
+      }
+    }
+  }
+}
+*/
+
 for (let i = 0; i < gridSize; i++) {
   for (let j = 0; j < gridSize; j++) {
-    newX = startX + i * 10;
-    newY = starY + j * 10;
+    newX = startX + i * 6;
+    newY = starY + j * 6;
 
     // fisheye
     res = deform(newX, newY); //[newX, newY] //
-    console.log(res);
-    circle(res[0], res[1], 3);
+    console.log(1 - res[2]);
+    scale = (1 - res[2]) * 1.4;
+
+    if (Math.random() < 0.3) {
+      star(res[0], res[1], 2 + scale);
+    } else {
+      circle(res[0], res[1], 1.3 + scale);
+    }
   }
 }
 
@@ -40,12 +70,28 @@ function deform(x, y) {
   factor = 1 + strength * (1 - norm) * (1 - norm);
   nx = fishX + dx * factor;
   ny = fishY + dy * factor;
-  return [nx, ny];
+  return [nx, ny, norm];
 }
 
 function circle(center_x, center_y, r) {
-  for (let i = 0; i < circleAccuracy; i++) {
+  t.penup();
+  t.goto(center_x, center_y);
+
+  for (let i = 0; i <= circleAccuracy; i++) {
     angle = (2 * Math.PI * i) / circleAccuracy;
+    x = center_x + r * Math.cos(angle);
+    y = center_y + r * Math.sin(angle);
+
+    t.goto(x, y);
+    t.pendown();
+
+    //  t.penup();
+  }
+}
+
+function star(center_x, center_y, r) {
+  for (let i = 0; i <= starAccuraccy; i++) {
+    angle = (2 * Math.PI * i) / starAccuraccy;
     x = center_x + r * Math.cos(angle);
     y = center_y + r * Math.sin(angle);
     t.penup();
