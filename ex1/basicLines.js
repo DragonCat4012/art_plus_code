884b4941f7520c21930011526f4aac7eee204f06
DragonCat4012 <frida.shivaa@gmail.com>
2026-04-16 12:24:56 +0200

    add comments

--- ex1/basicLines.js
+++ ex1/basicLines.js
@@ -19,13 +19,15 @@ const stepSize = 2; // min=1 max=100 step=10  Shape Placing
 const gridLenght = 860; // min=400 max=1000 step=50  Grid Length
 const chaosLevel = 0; // min=0, max=2, step=1, (Zero, Some, All)
 
-const chaosValues = [45, 30];
+const chaosValues = [45, 30]; // ordered chaos Angle values
 
 const t = new Turtle();
 t.penup();
 t.goto(-350, -150);
 t.pendown();
 
+// draw two angled lines to create a slanted grid
+// both do the same just different startpoints and left/right switched, could be moved into one singular func
 gridOne();
 gridTwo();
 
@@ -58,6 +60,7 @@ function gridTwo() {
     t.pendown();
   }
 }
+
 function gridOne() {
   t.penup();
   t.goto(-350, -150);
@@ -81,6 +84,7 @@ function gridOne() {
   }
 }
 
+// return the placed lines rotation based on selected chaos level
 function getChaos() {
   if (chaosLevel > 1) {
     return Math.floor(Math.random() * 361);
@@ -89,14 +93,13 @@ function getChaos() {
 
     if (Math.random() < 0.5) {
       return chaosValues[0];
-      // return chaosValues[chaosLevel+34]
     }
-
     return chaosValues[chaosLevel];
   }
   return chaosValues[chaosLevel];
 }
 
+// Draw the lines on the grid
 function abc(x, y) {
   const angle = getChaos();
   const prevHeading = t.heading();
@@ -106,7 +109,6 @@ function abc(x, y) {
   t.forward(10);
   t.penup();
   t.backward(5);
-  //t.left(angle);
   t.setheading(prevHeading);
   t.pendown();
 }
